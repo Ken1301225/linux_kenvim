55696954d720058649a1d75ea44f2767e420ea49
Kenlite <1521993813@qq.com>
2026-04-09 16:23:59 +0800

    add function about lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,7 +1,11 @@
 return {
     "nvimdev/lspsaga.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     cmd = "Lspsaga",
     opts = {
+        symbol_in_winbar = {
+            enable = true
+        },
         finder = {
             keys = {
                 toggle_or_open = "<CR>"
@@ -12,8 +16,12 @@ return {
         { "<leader>lr", ":Lspsaga rename<CR>" },
         { "<leader>lc", ":Lspsaga code_action<CR>" },
         { "<leader>ld", ":Lspsaga goto_definition<CR>" },
+        { "<leader>lD", ":Lspsaga peek_definition<CR>" },
         { "<leader>lh", ":Lspsaga hover_doc<CR>" },
         { "<leader>lR", ":Lspsaga finder<CR>" },
+        { "<leader>lo", ":Lspsaga outline<CR>" },
+        { "<leader>le", ":Lspsaga show_line_diagnostics<CR>" },
+        { "<leader>li", ":Lspsaga incoming_calls<CR>" },
         { "<leader>ln", ":Lspsaga diagnostic_jump_next<CR>" },
         { "<leader>lp", ":Lspsaga diagnostic_jump_prev<CR>" },
     }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,9 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = {"nvim-tree/nvim-web-devicons"},
     opts = {
+        view = {
+            width = 35
+        },
         actions = {
             open_file = {
                 quit_on_open = true
@@ -9,6 +12,8 @@ return {
         }
     },
     keys = {
-        {"<leader>uf" , ":NvimTreeToggle<CR>" }
+        {"<leader>uf", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree"},
+        {"<leader>u>", "<cmd>NvimTreeResize +5<CR>", desc = "NvimTree wider"},
+        {"<leader>u<", "<cmd>NvimTreeResize -5<CR>", desc = "NvimTree narrower"}
     }
 }
